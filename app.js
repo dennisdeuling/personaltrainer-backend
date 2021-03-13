970282fdda96b34f59a0a40e45da1c5f2c4710ca
dennis <>
2021-03-13 10:21:38 +0100

    index file

--- app.js
+++ app.js
@@ -69,9 +69,6 @@ app.use(passport.session());
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-/*const index = require('./routes/index');
-app.use('/', index);*/
-
 const authentication = require('./routes/auth-routes');
 app.use('/api', authentication);
 
